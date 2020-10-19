b9aa451e08277173bb1ca07e982672ea957ba4ee
Rohit Rohit <rohit.rohit@anz.com>
2020-10-20 10:53:50 +1100

    Add goodbye script

---
+++ blackduck-report-downloader/blackduck-report-downloader.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+BASE_URL='https://blackduck.platform-services.services-platdev.x.gcpnp.anz'
+AUTHENTICATE='api/tokens/authenticate'
+
+TOKEN="token $1"
+COMPONENT_JSON_URL="$2"
+
+curl --request POST --url ${BASE_URL}/${AUTHENTICATE} --header "Authorization: $TOKEN" | jq -r '.bearerToken'
+#token=$(curl --request POST --url ${BASE_URL}/${AUTHENTICATE} --header "Authorization: $TOKEN" | jq -r '.bearerToken')
+#curl --request GET --url ${COMPONENT_JSON_URL} --header "Authorization: Bearer $token" | tee report.json
